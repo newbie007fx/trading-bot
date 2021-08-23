ed532319aec1e2680a46feab80728c0e04aba81f
novianto <novianto.newbie@gmail.com>
2021-08-23 19:19:52 +0700

    change threshold

--- app/services/crypto/trading_strategy/base_strategy.go
+++ app/services/crypto/trading_strategy/base_strategy.go
@@ -127,7 +127,7 @@ func checkCryptoAltCoinPrice() []models.BandResult {
 		}
 
 		result.Weight = analysis.CalculateWeight(result, masterCoinTrend)
-		if !analysis.IsIgnored(result) && result.Direction == analysis.BAND_UP && result.Weight > 2.05 {
+		if !analysis.IsIgnored(result) && result.Direction == analysis.BAND_UP && result.Weight > 1.95 {
 			altCoin = append(altCoin, *result)
 		}
 	}
