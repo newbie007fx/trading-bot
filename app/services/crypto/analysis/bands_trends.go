f31bd2e000a92ff953f0b9f109163209e53ed5a4
novianto <novianto.newbie@gmail.com>
2021-09-01 11:12:53 +0700

    try adjust again

--- app/services/crypto/analysis/bands_trends.go
+++ app/services/crypto/analysis/bands_trends.go
@@ -6,6 +6,7 @@ func CalculateTrends(data []models.Band) int8 {
 	lastCandle := data[len(data)-1].Candle
 
 	highestIndex, lowestIndex := 0, 0
+	var total float32 = 0
 	for i, val := range data {
 		if data[highestIndex].Candle.Close < val.Candle.Close {
 			highestIndex = i
@@ -14,9 +15,13 @@ func CalculateTrends(data []models.Band) int8 {
 		if data[lowestIndex].Candle.Close > val.Candle.Close {
 			lowestIndex = i
 		}
+
+		if i < len(data)-1 {
+			total += val.Candle.Close
+		}
 	}
 
-	average := (data[lowestIndex].Candle.Close + data[highestIndex].Candle.Close) / 2
+	average := total / float32(len(data)-2)
 	highestValueDifference := data[highestIndex].Candle.Close - average
 	lowestValueDifference := average - data[lowestIndex].Candle.Close
 
@@ -29,11 +34,12 @@ func CalculateTrends(data []models.Band) int8 {
 		percent = (difference / lowestValueDifference) * 100
 	}
 
-	if percent <= float32(25) {
+	if percent <= float32(30) {
 		return models.TREND_SIDEWAY
 	}
 
-	if lastCandle.Close > average {
+	averageTrend := (data[lowestIndex].Candle.Close + data[highestIndex].Candle.Close) / 2
+	if lastCandle.Close > averageTrend {
 		return models.TREND_UP
 	}
 
