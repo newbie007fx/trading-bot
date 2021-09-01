3b89330caee96b509b95864a99f4cfb83ad30e83
novianto <novianto.newbie@gmail.com>
2021-09-01 16:30:23 +0700

    adjust threshold again

--- app/services/crypto/analysis/bands_trends.go
+++ app/services/crypto/analysis/bands_trends.go
@@ -30,7 +30,7 @@ func CalculateTrends(data []models.Band) int8 {
 		percent = (lastCandle.Close / average) * 100
 	}
 
-	if percent >= float32(92) {
+	if percent >= float32(95) {
 		return models.TREND_SIDEWAY
 	}
 
