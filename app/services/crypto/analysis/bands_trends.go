479ccdd25155c55bc56115315bf33bff5bae39ba
novianto <novianto.newbie@gmail.com>
2021-09-28 08:30:04 +0700

    reduce shord trend

--- app/services/crypto/analysis/bands_trends.go
+++ app/services/crypto/analysis/bands_trends.go
@@ -244,7 +244,7 @@ func getTrendShort(baseLine, fistAvg, secondAvg float32) int8 {
 		percent = (firstPointValue / lastPointValue) * 100
 	}
 
-	if percent >= 90 {
+	if percent >= 85 {
 		return models.TREND_SIDEWAY
 	}
 
