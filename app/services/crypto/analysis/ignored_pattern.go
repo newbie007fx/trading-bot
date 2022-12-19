62aab63490012926229f770809ee4ba72d7eac31
novianto_FLIP <novianto>
2022-12-19 08:51:13 +0700

    adjust ignore pattern

--- app/services/crypto/analysis/ignored_pattern.go
+++ app/services/crypto/analysis/ignored_pattern.go
@@ -345,10 +345,23 @@ func ApprovedPattern(short, mid, long models.BandResult, currentTime time.Time, 
 		return false
 	}
 
-	if ((long.AllTrend.ShortTrend == models.TREND_DOWN || isHasOpenOrCloseBelowLower(long.Bands[bandLen-4:])) && long.Position == models.BELOW_SMA) ||
-		((mid.AllTrend.SecondTrend == models.TREND_DOWN || isHasOpenOrCloseBelowLower(mid.Bands[bandLen-4:])) && mid.Position == models.BELOW_SMA) ||
-		((short.AllTrend.SecondTrend == models.TREND_DOWN || isHasOpenOrCloseBelowLower(short.Bands[bandLen-4:])) && short.Position == models.BELOW_SMA) {
+	if (long.AllTrend.ShortTrend == models.TREND_DOWN || isHasOpenOrCloseBelowLower(long.Bands[bandLen-4:])) && long.Position == models.BELOW_SMA {
 		log.Println("skipped12")
+		return false
+	}
+
+	if (mid.AllTrend.SecondTrend == models.TREND_DOWN || isHasOpenOrCloseBelowLower(mid.Bands[bandLen-4:])) && mid.Position == models.BELOW_SMA {
+		log.Println("skipped13")
+		return false
+	}
+
+	if (short.AllTrend.SecondTrend == models.TREND_DOWN || isHasOpenOrCloseBelowLower(short.Bands[bandLen-4:])) && short.Position == models.BELOW_SMA {
+		log.Println("skipped14")
+		return false
+	}
+
+	if long.Position == models.BELOW_SMA && mid.Position == models.BELOW_SMA {
+		log.Println("skipped15")
 		return false
 	}
 
