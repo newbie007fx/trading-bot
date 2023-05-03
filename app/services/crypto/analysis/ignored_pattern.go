ccffe8f38004d723e3ba04507a03fb6b039f39eb
novianto <novianto>
2023-05-03 08:42:17 +0700

    adjust ignore pattern

--- app/services/crypto/analysis/ignored_pattern.go
+++ app/services/crypto/analysis/ignored_pattern.go
@@ -415,7 +415,7 @@ func approvedPatternOnCompleteCheck(short, mid, long models.BandResult, currentT
 	midLastBand := mid.Bands[bandLen-1]
 	shortLastBand := short.Bands[bandLen-1]
 
-	if isSolidBand(shortLastBand) && CountBadBand(short.Bands[bandLen-4:bandLen-1], false) < 3 {
+	if (isSolidBand(shortLastBand) && CountBadBand(short.Bands[bandLen-4:bandLen-1], false) < 3) || isNoNeedDoubleCheck {
 		if isLastBandDoublePreviousHeigest(short.Bands) || isNoNeedDoubleCheck {
 			if isUpperHeadMoreThanUpperBody(midLastBand) && isUpperHeadMoreThanUpperBody(longLastBand) {
 				ignoredReason = "mid and long upper head more than body"
